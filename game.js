d73f7b970a2f2c61372354472f5a22a7608a77f7
Sambhav <223879753+motordlx@users.noreply.github.com>
2026-04-26 21:56:13 +0530

    changes done to the dom

--- game.js
+++ game.js
@@ -1,6 +1,13 @@
 
 let computerScore=0;
 let userScore=0;
+
+let rs=document.getElementById("round");
+
+let rock =document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissor = document.getElementById("scissor");
+let end=document.getElementById("end");
 function getComputerChoice(){
 
      const array=["rock",'paper','scissor'];
@@ -8,75 +15,71 @@ function getComputerChoice(){
    return array[Math.floor(Math.random()*array.length)];
 }
 
-function getUserChoice(){
+let userChoice="";
+rock.addEventListener("click",()=>{userChoice="Rock";playRound()});
 
-    let userChoice=prompt("Choose between Rock,Paper,Scissor:");
+paper.addEventListener("click",()=>{userChoice="Paper";playRound()});
 
-    return userChoice;
-}
+scissor.addEventListener("click",()=>{userChoice="Scissor";playRound()});
+
+
 
 function playRound(){
 
    let  computerChoice=getComputerChoice();
-    let userChoice=getUserChoice();
 
     if(userChoice==='Rock'){
-        if(computerChoice=='scissor'){
-            console.log("user won this round");
+        if(computerChoice==='scissor'){
+            rs.innerText="user won this round "+computerChoice;
             userScore++;
         }
-        else if(computerChoice=='paper') {
-            console.log("computer won this round");
+        else if(computerChoice==='paper') {
+            rs.innerText="computer won this round "+computerChoice;
             computerScore++;
         }
         else{
-            console.log("draw this round");
+           rs.innerText="draw this round "+computerChoice;
         }
 
     }
     else if(userChoice==='Scissor'){
-        if(computerChoice=='paper'){
-            console.log("user won this round");
+        if(computerChoice==='paper'){
+            rs.innerText="user won this round "+computerChoice;
             userScore++;
         }
-        else if(computerChoice=='Rock') {
-            console.log("computer won this round");
-            computerScore++;
-        }
-        else console.log("drawn this round");
-
-    }
-    else{
-        if(computerChoice=='rock'){
-            console.log("user won this round");
-            userScore++;
-        }
-        else if(computerChoice=='scissor') {
-            console.log("computer won this round");
+        else if(computerChoice==='Rock') {
+            rs.innerText="computer won this round "+computerChoice;
             computerScore++;
         }
         else{
-            console.log("drawn this round");
+            rs.innerText="draw this round "+computerChoice;
+        }
+
+    }
+    else{
+        if(computerChoice==='rock'){
+            rs.innerText="user won this round "+computerChoice;
+            userScore++;
+        }
+        else if(computerChoice==='scissor') {
+            rs.innerText="computer won this round "+computerChoice;
+            computerScore++;
+        }
+        else{
+            rs.innerText="draw this round "+computerChoice;
         }
     }
 }
+ let en =document.getElementById("en");
 
-function game(){
-    for(let i=0;i<5;i+=1)
-    {
-        playRound();
-    }
 
-    if(computerScore>userScore){
-        console.log("computer won "+ computerScore);
+end.addEventListener("click",()=> {
+    if (computerScore > userScore) {
+        en.innerText="comoputer won this game"
+    } else if (computerScore < userScore) {
+        en.innerText="user won this game"
+    } else {
+        en.innerText="draw";
     }
-    else if(computerScore<userScore){
-        console.log("user won "+userScore);
-    }
-    else{
-        console.log("Draw");
-    }
-
-}
-game();
+});
 
